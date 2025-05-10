d8c0352e1f62cde4a755d3d1f30d2509d167e72c
Carlos Robert <149803558+dev-carl-robert@users.noreply.github.com>
2025-05-10 13:22:45 -0300

    produtos e links 10/05/2025

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const produtos = [
+const modaInfantil = [
     {
         imagem: "produtos\\infantil 0.jpg",
         nome: "Conjunto Infantil",
         preco: "25.00",
-        preco_cartao:"27.00",
+        preco_cartao: "27.00",
         descricao: "Algodão ",
         identificador: "MI001"
     },
@@ -12,7 +12,7 @@ const produtos = [
         imagem1: "produtos\\infantil 1,1.jpg",
         nome: "Cueca Infantil",
         preco: "4.50",
-        preco_cartao:"5.00",
+        preco_cartao: "5.00",
         descricao: "personagem superman e homem aranha",
         identificador: "MI002"
     },
@@ -22,7 +22,7 @@ const produtos = [
         imagem2: "produtos\\infantil 2,2.jpg",
         nome: "calcinha Infantil",
         preco: "4.50",
-        preco_cartao:"5.00",
+        preco_cartao: "5.00",
         descricao: "Algodão PMG/GG",
         identificador: "MI003"
     },
@@ -31,7 +31,7 @@ const produtos = [
         imagem1: "produtos\\infantil 3,1.jpg",
         nome: "Calcinha Infantil",
         preco: "6.50",
-        preco_cartao:"7.00",
+        preco_cartao: "7.00",
         descricao: "Algodão PMG/GG",
         identificador: "MI004"
     },
@@ -39,7 +39,7 @@ const produtos = [
         imagem: "produtos\\infantil 4.jpg",
         nome: "Vestido Infantil ",
         preco: "25.00",
-        preco_cartao:"27.00",
+        preco_cartao: "27.00",
         descricao: "PMG Não garantimos a mesma estampa em todos os tamanhos.",
         identificador: "MI005"
     },
@@ -47,7 +47,7 @@ const produtos = [
         imagem: "produtos\\infantil 5.jpg",
         nome: "Vestido Infantil",
         preco: "25.00",
-        preco_cartao:"27.00",
+        preco_cartao: "27.00",
         descricao: "TAM: PMG Não garantimos mesma estampa em todos os tamanhos.",
         identificador: "MI006"
     },
@@ -56,7 +56,7 @@ const produtos = [
         imagem1: "produtos\\infantil 6,1.jpg",
         nome: "top infantil",
         preco: "8.50",
-        preco_cartao:"9.00",
+        preco_cartao: "9.00",
         descricao: "TAM: PMG/GG",
         identificador: "MI007"
     },
@@ -64,7 +64,7 @@ const produtos = [
         imagem: "produtos\\infantil 7.jpg",
         nome: "Cueca Infantil",
         preco: "10.00",
-        preco_cartao:"11.00",
+        preco_cartao: "11.00",
         descricao: "TAM: PMG/GG",
         identificador: "MI008"
     },
@@ -72,7 +72,7 @@ const produtos = [
         imagem: "produtos\\infantil 8.jpg",
         nome: "Kit Meia Infantil",
         preco: "8.00",
-        preco_cartao:"9.00",
+        preco_cartao: "9.00",
         descricao: "",
         identificador: "MI009"
     },
@@ -81,7 +81,7 @@ const produtos = [
         imagem1: "produtos\\infantil 9,1.jpg",
         nome: "Kit Meia Infantil",
         preco: "16.50",
-        preco_cartao:"18.00",
+        preco_cartao: "18.00",
         descricao: "",
         identificador: "MI010"
     },
@@ -89,7 +89,7 @@ const produtos = [
         imagem: "produtos\\infantil 10.jpg",
         nome: "SHort Infantil",
         preco: "16.00",
-        preco_cartao:"17.00",
+        preco_cartao: "17.00",
         descricao: "1 a 8 anos",
         identificador: "MI011"
     },
@@ -106,7 +106,7 @@ const produtos = [
         imagem9: "produtos\\infantil 11,9.jpg",
         nome: "Vestido Infantil",
         preco: "25.00",
-        preco_cartao:"27.00",
+        preco_cartao: "27.00",
         descricao: "",
         identificador: "MI012"
     },
@@ -114,7 +114,7 @@ const produtos = [
         imagem: "produtos\\infantil 12.jpg",
         nome: "Bermuda Infantil",
         preco: "40.00",
-        preco_cartao:"42.00",
+        preco_cartao: "42.00",
         descricao: "",
         identificador: "MI013"
     },
@@ -124,7 +124,7 @@ const produtos = [
         imagem2: "produtos\\infantil 13,2.jpg",
         nome: "Kit Calcinha Diamante (Infantil) ",
         preco: "130.00",
-        preco_cartao:"120",
+        preco_cartao: "120",
         descricao: "kit com 7 peças PMG/GG",
         identificador: "MI014"
     },
@@ -137,7 +137,7 @@ const produtos = [
         imagem5: "produtos\\infantil 14,5.jpg",
         nome: "Baby Doll Infantil",
         preco: "25.00",
-        preco_cartao:"27.00",
+        preco_cartao: "27.00",
         descricao: "tamanho P ao EX; Não tem no atacado",
         identificador: "MI015"
     },
@@ -146,25 +146,148 @@ const produtos = [
         imagem1: "produtos\\infantil 15,1.jpg",
         nome: "Conjunto Juvenil",
         preco: "16.00",
-        preco_cartao:"17.00",
+        preco_cartao: "17.00",
         descricao: "PMG/GG",
-        identificador: "MI015"
-    }
+        identificador: "MI016"
+    },
+    {
+        imagem: "produtos\\infantil 16.jpg",
+        imagem1: "produtos\\infantil 16,1.jpg",
+        imagem2: "produtos\\infantil 16,2.jpg",
+        imagem3: "produtos\\infantil 16,3.jpg",
+        imagem4: "produtos\\infantil 16,4.jpg",
+        imagem5: "produtos\\infantil 16,5.jpg",
+        nome: "Kit Cueca Diamante",
+        preco: "164.00",
+        preco_cartao: "176.00",
+        descricao: "Kit com 4 peças personalizadas Infantil; tamanho PMG/GG",
+        identificador: "MI017"
+    },
+    {
+        imagem: "produtos\\infantil 17.jpg",
+        nome: "Cueca Infantil",
+        preco: "10.00",
+        preco_cartao: "11.00",
+        descricao: "Tecido algodão",
+        identificador: "MI018"
+    },
+    {
+        imagem: "produtos\\infantil 18.jpg",
+        nome: "Camisa Infantil",
+        preco: "20.00",
+        preco_cartao: "24.00",
+        descricao: "Tecido algodão; de 1 a 16 anos",
+        identificador: "MI019"
+    },
+    {
+        imagem: "produtos\\infantil 19.jpg",
+        nome: "Toalha Infantil",
+        preco: "26.00",
+        preco_cartao: "28.00",
+        descricao: "Tecido algodão, tamanho 1,30x70cm",
+        identificador: "MI020"
+    },
+    {
+        imagem: "produtos\\infantil 20.jpg",
+        nome: "Calcinha Infantil",
+        preco: "6.50",
+        preco_cartao: "7.50",
+        descricao: "tamanho PMG/GG",
+        identificador: "MI021"
+    },
+    {
+        imagem: "produtos\\infantil 21.jpg",
+        nome: "Cueca Lupo Infantil",
+        preco: "23.00",
+        preco_cartao: "25.00",
+        descricao: "tamanho PP/P/M/G/GG",
+        identificador: "MI022"
+    },
+    {
+        imagem: "produtos\\infantil 22.jpg",
+        nome: "Calcinha Box Infantil",
+        preco: "5.50",
+        preco_cartao: "6.00",
+        descricao: "",
+        identificador: "MI023"
+    },
+    {
+        imagem: "produtos\\infantil 23.jpg",
+        imagem1: "produtos\\infantil 23,1.jpg",
+        imagem2: "produtos\\infantil 23,2.jpg",
+        imagem3: "produtos\\infantil 23,3.jpg",
+        imagem4: "produtos\\infantil 23,4.jpg",
+        imagem5: "produtos\\infantil 23,5.jpg",
+        imagem: "produtos\\infantil 23,6.jpg",
+        imagem: "produtos\\infantil 23,7.jpg", 
+        imagem: "produtos\\infantil 23,8.jpg",
+        nome: "Baby Doll Infantil",
+        preco: "16.50",
+        preco_cartao: "19.00",
+        descricao: "tamanho PMG/G",
+        identificador: "MI024"
+    },
+    {
+        imagem: "produtos\\infantil 24.jpg",
+        nome: "Calcinha Infantil",
+        preco: "6.50",
+        preco_cartao: "7.00",
+        descricao: "tamanho PMG/GG",
+        identificador: "MI025"
+    },
+    {
+        imagem: "produtos\\infantil 25.jpg",
+        nome: "Calcinha Infantil",
+        preco: "7.50",
+        preco_cartao: "8.00",
+        descricao: "Tecido algodão Tamanhos P/M/G/GG",
+        identificador: "MI026"
+    },
+    {
+        imagem: "produtos\\infantil 26.jpg",
+        nome: "Kit Calcinha Diamante",
+        preco: "164.00",
+        preco_cartao: "176.00",
+        descricao: "Kit com 7",
+        identificador: "MI027"
+    },
+    {
+        imagem: "produtos\\infantil 27.jpg",
+        nome: "Rede berço",
+        preco: "130.00",
+        preco_cartao: "160.00",
+        descricao: "",
+        identificador: "MI028"
+    },
+    {
+        imagem: "produtos\\infantil 28.jpg",
+        nome: "Kit Cueca Diamante",
+        preco: "164.00",
+        preco_cartao: "176.00",
+        descricao: "Tecido algodão; kit com 4 peças personalizadas infantil",
+        identificador: "MI029"
+    },
 ];
 
 const container = document.querySelector(".produtos");
-const modelo = container.querySelector(".produto");
 
-// Remove o produto de exemplo do HTML depois de clonar
+// Remover todos os produtos de exemplo antes de adicionar novos produtos
 container.innerHTML = "";
 
-produtos.forEach(prod => {
-    const clone = modelo.cloneNode(true);
-    clone.querySelector(".imagem").src = prod.imagem;
-    clone.querySelector(".imagem").alt = prod.nome;
-    clone.querySelector(".nome_produto").textContent = prod.nome;
-    clone.querySelector(".preco").textContent = `R$${prod.preco}`;
-    clone.querySelector(".descricao").textContent = prod.descricao;
+// Adicionar os produtos dinamicamente
+modaInfantil.forEach(prod => {
+    // Cria um novo produto dinamicamente
+    const divProduto = document.createElement("div");
+    divProduto.classList.add("produto");
 
-    container.appendChild(clone);
+    // Cria o conteúdo do produto
+    divProduto.innerHTML = `
+        <img class="imagem" src="${prod.imagem}" alt="${prod.nome}">
+        <h3 class="nome_produto">${prod.nome}</h3>
+        <button class="preco">R$${prod.preco}</button>
+        <p class="descricao">${prod.descricao}</p>
+    `;
+
+    // Adiciona o produto criado ao contêiner
+    container.appendChild(divProduto);
 });
